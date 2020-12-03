192b3b8241ffe286eb94f75e88fa48271f6809bc
MajBearclaw <1978145571@qq.com>
2020-12-03 19:36:30 +0800

    多写了一点点

--- src/views/system/user.vue
+++ src/views/system/user.vue
@@ -1,5 +1,41 @@
 <template>
+  <div class="content_container">
+    <div>
+      <el-form :label-position="labelPosition" :inline="true" :model="form" class="demo-form-inline" size="mini">
+        <el-form-item label="关键字">
+          <el-input v-model="param.keyword" placeholder="请输入关键字" clearable></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button v-for="item in bttns" type="primary" size="mini" :icon="item.icon"
+                     @click="handleMethod(item.methodd)">{{ item.name }}
+          </el-button>
+        </el-form-item>
+      </el-form>
+    </div>
 
+    <div>
+      <tree-table :data="data" :columns="columns" :options="tableOption" border/>
+    </div>
+
+    <el-dialog :title="dialogName" :visible.sync="dialogFormVisible" @close="" center>
+      <el-form :model="form" ref="dialogForm" :rules="formRules" :label-position="labelPosition" label-width="100px"
+               size="mini">
+        <el-form-item label="姓名" prop="name">
+          <el-input v-model="form.name" placeholder="姓名" style="width: auto"></el-input>
+        </el-form-item>
+        <el-form-item label="性别">-->
+          <el-radio-group v-model="form.">
+            <el-radio :label="1">是</el-radio>
+            <el-radio :label="0">否</el-radio>
+          </el-radio-group>
+        </el-form-item>
+      </el-form>
+      <div>
+        <el-button @click="dialogFormVisible = false">取 消</el-button>
+        <el-button type="primary" @click="submitForm">确 定</el-button>
+      </div>
+    </el-dialog>
+  </div>
 </template>
 
 <script>
@@ -16,6 +52,30 @@ export default {
     treeTable,
   },
   data() {
+    let updateOpen = (row) => {
+      delete row.children;
+      row.pid = null;
+      // this.form = copyProperties(row,this.form);
+      // console.log(this.form);
+      this.form = JSON.parse(JSON.stringify(row));//解除数据绑定
+      //this.form.pid = null;
+      this.form.isUpdate = row.isUpdate == true ? 1 : 0;
+      this.form.isDelete = row.isDelete == true ? 1 : 0;
+
+      this.codeReadonly = true;
+      this.dialogName = "修改";
+      this.dialogFormVisible = true;
+      this.$refs.dialogForm.clearValidate();//清除校验结果
+    }
+    let deleteOption = (row) => {
+      this.delete(row);
+    }
+    let isUpdateShow = (row) => {
+      return row.isUpdate == 1;
+    }
+    let isDeleteShow = (row) => {
+      return row.isDelete == 1 && (!row.children || row.children.length == 0);
+    }
     return {
       bttns: [],
       options: [],
@@ -25,8 +85,11 @@ export default {
       data: [],
       formRules: {
         name: [{required: true, trigger: 'blur', message: "请输入姓名"}],
-        description: [{required: true, trigger: 'blur', message: "请选择性别"}],
-        //////待补充
+        gender: [{required: true, trigger: 'blur', message: "请选择性别"}],
+        jobNumber: [{required: true, trigger: 'blur', message: "请输入工号"}],
+        career: [{required: true, trigger: 'blur', message: "请输入职位"}],
+        role: [{required: true, trigger: 'blur', message: "请输入角色"}],
+        phone: [{required: true, trigger: 'blur', message: "请输入手机号"}],
       },
       param:{
         // 检索用的关键字
@@ -44,8 +107,28 @@ export default {
           value: 'name'
         },
         {
-          text: '创建时间',
-          value: 'createTime'
+          text: '性别',
+          value: 'gender'
+        },
+        {
+          text: '工号',
+          value: 'jobNumber'
+        },
+        {
+          text: '职位',
+          value: 'career'
+        },
+        {
+          text: '角色',
+          value: 'role'
+        },
+        {
+          text: '手机号',
+          value: 'phone'
+        },
+        {
+          text: '状态',
+          value: 'state'
         },
       ],
       tableOption: [
@@ -54,17 +137,20 @@ export default {
           onclick: updateOpen,
           isShow: isUpdateShow,
         },
-        {
-          text: '删除',
-          onclick: deleteOption,
-          isShow: isDeleteShow,
-        }
+        // {
+        //   text: '删除',
+        //   onclick: deleteOption,
+        //   isShow: isDeleteShow,
+        // }
       ],
     }
   },
   methods: {
     handleMethod(ms) {
       this[ms]();
+    },
+    search() {
+      this.getRoleList();
     },
     add() {
       this.form = {
